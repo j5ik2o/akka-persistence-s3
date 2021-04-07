ba7e98194cc35065e2a03c746876c807ce4561dd
Scala Steward <me@scala-steward.org>
2021-04-07 20:30:56 +0200

    Update akka-persistence, ... to 2.6.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scala211Version    = "2.11.12"
     val scala212Version    = "2.12.10"
     val scala213Version    = "2.13.1"
-    val akka26Version      = "2.6.10"
+    val akka26Version      = "2.6.14"
     val akka25Version      = "2.5.32"
     val scalaTest32Version = "3.2.5"
     val scalaTest30Version = "3.0.8"
