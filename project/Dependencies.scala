fb80de56338560b6250c580a3bff92e8ea92a519
Junichi Kato <j5ik2o@gmail.com>
2021-05-21 09:16:12 +0900

    update scalafix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val scala212Version  = "2.12.10"
-    val scala213Version  = "2.13.5"
+    val scala213Version  = "2.13.6"
     val scala3Version    = "3.0.0"
     val akkaVersion      = "2.6.10"
     val scalaTestVersion = "3.2.9"
